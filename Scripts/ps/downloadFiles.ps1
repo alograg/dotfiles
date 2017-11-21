e18f603e67a6d02fd99573fa3e8e0fbf4d0cf691
Henry <alograg@gmail.com>
2017-11-21 11:01:48 -0600

    Nueva configuracion Script de descarga (PowerShell)

---
+++ Scripts/ps/downloadFiles.ps1
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+function Get-FileFromUri {  
+    param(  
+        [parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+        [string]
+        [Alias('Uri')]
+        $Url,
+        [parameter(Mandatory=$false, Position=1)]
+        [string]
+        [Alias('Folder')]
+        $FolderPath
+    )
+    process {
+        try {
+            # resolve short URLs
+            $req = [System.Net.HttpWebRequest]::Create($Url)
+            $req.Method = "HEAD"
+            $response = $req.GetResponse()
+            $fUri = $response.ResponseUri
+            $filename = [System.IO.Path]::GetFileName($fUri.LocalPath);
+            $response.Close()
+            # download file
+            $destination = (Get-Item -Path ".\" -Verbose).FullName
+            if ($FolderPath) { $destination = $FolderPath }
+            if ($destination.EndsWith('\')) {
+                $destination += $filename
+            } else {
+                $destination += '\' + $filename
+            }
+            $webclient = New-Object System.Net.webclient
+            $webclient.downloadfile($fUri.AbsoluteUri, $destination)
+            write-host -ForegroundColor DarkGreen "downloaded '$($fUri.AbsoluteUri)' to '$($destination)'"
+        } catch {
+            write-host -ForegroundColor DarkRed $_.Exception.Message
+        }  
+    }  
+}
+
+Get-Content forDownload.txt | % {
+    Get-FileFromUri $_
+}
