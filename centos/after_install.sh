ad90923dfa07f2d93df5534cd7ab60e5a1fe29aa
root <root@alograg.acer>
2018-02-03 14:17:43 +0100

    Mejoras par gnome

--- centos/after_install.sh
+++ centos/after_install.sh
@@ -10,12 +10,13 @@ yum -y install net-tools
 # Links
 yum -y install links
 # Git
-yum --disablerepo=base,updates --enablerepo=rpmforge-extras update git
+yum -y install http://opensource.wandisco.com/centos/6/git/x86_64/wandisco-git-release-6-1.noarch.rpm
+yum -y install git
 # Apache/PHP/MariaDb
 yum -y install httpd php php-devel php-common php-soap php-gd mariadb-server mariadb
-firewall-cmd -permanent --add-service=http
-firewall-cmd -permanent --add-service=mysql
-firewall-cmd -permanent -add-port=3221/tcp
+firewall-cmd --permanent --add-service=http
+firewall-cmd --permanent --add-service=mysql
+firewall-cmd --permanent --add-port=3221/tcp
 firewall-cmd --reload
 systemctl restart httpd.service
 systemctl start httpd.service
@@ -33,20 +34,23 @@ systemctl restart sshd.service
 yum -y install nmap
 # XWindow
 yum -y groupinstall "X Window System"
-yum -y install gnome-classic-session gnome-terminal nautilus-open-terminal control-center liberation-mono-fonts
+yum -y install gnome-classic-session gnome-terminal nautilus-open-terminal control-center liberation-mono-fonts gnome-tweak-tool
 unlink /etc/systemd/system/default.target
 ln -sf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
+# remve bottom bar
+rm -R /usr/share/gnome-shell/extensions/window-list@gnome-shell-extensions.gcampax.github.com
+yum -y install gnome-shell-extension-*
 # Chrome
-echo -e "[google-chrome]">/etc/yum.repos.d/google-chrome.repo
-echo -e "name=google-chrome">>/etc/yum.repos.d/google-chrome.repo
-echo -e "baseurl=http://dl.google.com/linux/chrome/rpm/stable/$basearch">>/etc/yum.repos.d/google-chrome.repo
-echo -e "enabled=1">>/etc/yum.repos.d/google-chrome.repo
-echo -e "gpgcheck=1">>/etc/yum.repos.d/google-chrome.repo
-echo -e "gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub">>/etc/yum.repos.d/google-chrome.repo
-yum -y install google-chrome-stable
-# NodeJS
-curl --silent --location https://rpm.nodesource.com/setup_7.x | bash -
-yum -y install nodejs npm
+#echo -e "[google-chrome]">/etc/yum.repos.d/google-chrome.repo
+#echo -e "name=google-chrome">>/etc/yum.repos.d/google-chrome.repo
+#echo -e "baseurl=http://dl.google.com/linux/chrome/rpm/stable/$basearch">>/etc/yum.repos.d/google-chrome.repo
+#echo -e "enabled=1">>/etc/yum.repos.d/google-chrome.repo
+#echo -e "gpgcheck=1">>/etc/yum.repos.d/google-chrome.repo
+#echo -e "gpgkey=https://dlssl.google.com/linux/linux_signing_key.pub">>/etc/yum.repos.d/google-chrome.repo
+#yum -y install google-chrome-stable
+curl https://intoli.com/install-google-chrome.sh | bash
+# NodeJS Version manager
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 # 7Zip
 yum -y install p7zip p7zip-plugins p7zip-gui
 # DefaultApps
@@ -90,6 +94,6 @@ fc-cache -f
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 yum check-update
-yum install code
+yum -y install code
 # Reboot
 reboot
