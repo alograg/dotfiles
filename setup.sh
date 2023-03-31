1a2353373ab93e4611cbb8177208a324717c9c63
alograg <alograg@gmail.com>
2023-03-31 11:19:09 +0200

    fix(git): :wrench: Set alias, color, merge, push and credentials config

--- setup.sh
+++ setup.sh
@@ -5,16 +5,16 @@
 cd ~
 
 sudo apt install -y \
-	git \
-	neovim \
-	vifm \
-	build-essential \
-	p7zip-full \
-	p7zip-rar \
-	fuseiso \
-	sshfs \
-	libarchive-dev libfuse-dev \
-	golang
+  git \
+  neovim \
+  vifm \
+  build-essential \
+  p7zip-full \
+  p7zip-rar \
+  fuseiso \
+  sshfs \
+  libarchive-dev libfuse-dev \
+  golang
 git clone https://github.com/google/fuse-archive.git
 cd fuse-archive || exit
 sudo make install
@@ -23,43 +23,35 @@ rm -fR fuse-archive
 
 # Configuracion de respaldo de configuracion
 
-git clone --bare https://github.com/alograg/dotfiles.git $HOME/.cfg
-
+if [ "$(type -t config)" = "" ]; then
 config() {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree="$HOME" $@
 }
+fi
 
-mkdir -p .config-backup
-config checkout
-if [ $? = 0 ]; then
+if [ ! -d $HOME/.cfg ] ; then
+  git clone --bare https://github.com/alograg/dotfiles.git $HOME/.cfg
+  mkdir -p .config-backup
+  config checkout
+  if [ $? = 0 ]; then
   echo "Checked out config.";
-else
+  else
   echo "Backing up pre-existing dot files.";
   config checkout 2>&1 | grep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-fi;
-config checkout
-config config status.showUntrackedFiles no
+  fi;
+  config checkout
+  config config status.showUntrackedFiles no
+fi
 
 # Configuracion de git
 
-git config --global user.name "$USER"
-read -p "Enter your git email [$USER@exemple.com]: " correo
-correo=${correo:-"$USER@exemple.com"}
-git config --global user.email "$correo"
-git config --global core.editor "nvim"
-git config --global color.ui "auto"
-git config --global core.filemode false
-git config --global alias.st status
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global alias.ci commit
-git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
-git config --global core.excludesfile ~/.config/.gitignore_global
-#git config --global commit.gpgsign true
-git config --global merge.defaultToUpstream recursive
-git config --global push.default simple
-git config --global core.preloadindex true
-git config --global core.fscache true
-git config --global credential.helper cache
+echo -n -e "Change the user and git mail? (y/\e[4mn\e[0m)"
+read -p ": " yn
+if [ "$yn" = "y" ]; then
+  git config --global user.name "$USER"
+  read -p "Enter your git email [$USER@exemple.com]: " correo
+  correo=${correo:-"$USER@exemple.com"}
+  git config --global user.email "$correo"
+fi
 
 # git lfs install
